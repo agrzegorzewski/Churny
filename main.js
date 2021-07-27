7c9877c85ae28b74614836302b49ea82fa8090a3
Turipuru-chan <triplemonkey47@gmail.com>
2021-07-27 17:47:54 +0200

    small bugfixes

--- main.js
+++ main.js
@@ -5,7 +5,10 @@ function generateEmail(input) {
     let cardIssue = 0;
 
     let competition = [];
-    const regex = /no|none|None|-/;
+
+    const competitionRegex = /\bno\b|\bnone\b|\bNone\b|^-$/;
+
+    document.getElementById('output').innerHTML = "";
 
     const cardProblems = ["call_issuer", "card_type_not_accepted", "declined", "expired_card", "fraud_security_code", "fraud_stolen_card", "insufficient_funds", "invalid_account_number", "invalid_transaction", "no_billing_information", "processor_unavailable", "restricted_card"];
 
@@ -13,42 +16,32 @@ function generateEmail(input) {
         inputTable[index] = inputTable[index].split("\t");
     }
 
-    for (let index = 0; index < inputTable.length; index++) {
-        if (inputTable[index][0] == "yes") manuallyClosed++;
-        if (inputTable[index][1] == "yes") cardIssue++;
+    for (let row in inputTable) {
+        if (inputTable[row][0] == "yes") manuallyClosed++;
+        if (inputTable[row][1] == "yes") cardIssue++;
     }
 
-    inputTable.forEach(element => {
-        if (element.length > 6) {
-            for (let index = 6; index < element.length; index++) {
-                element[5] += " " + element[index];
-            }
-        }
-    });
+    concatCompetition(inputTable);
 
     document.getElementById('output').innerHTML += "Churns: <br><br>Manually closed: " + manuallyClosed + "<br>Card issues: " + cardIssue + "<br><br>";
 
     cardProblems.forEach(element => {
         let n = 0;
-        for (let index = 0; index < inputTable.length; index++) {
-            if (inputTable[index][2] == element) n++;
+        for (let row in inputTable) {
+            if (inputTable[row][2] == element) n++;
         }
         if (n > 0) document.getElementById('output').innerHTML += element + ": " + n + "<br>";
     });
 
     inputTable.forEach(element => {
-        if (!regex.test(element[5])) competition.push(element[5])
+        if (!(competitionRegex.test(element[5]))) competition.push(element[5])
     });
 
     competition.sort();
 
-    document.getElementById('output').innerHTML += "<br>Competition:" + competition.join("<br>");
+    document.getElementById('output').innerHTML += "<br>Competition:<br>" + competition.join("<br>");
 
     console.log(inputTable);
-    /*  console.log("manual: " + manuallyClosed);
-        console.log("card: " + cardIssue);
-        console.log(competition.sort()); */
-
 }
 
 function copyEmail() {
@@ -58,4 +51,14 @@ function copyEmail() {
     window.getSelection().addRange(range);
     document.execCommand('copy');
     window.getSelection().removeAllRanges();
+}
+
+function concatCompetition(table) {
+    table.forEach(element => {
+        if (element.length > 6) {
+            for (let index = 6; index < element.length; index++) {
+                element[5] += " " + element[index];
+            }
+        }
+    })
 }
